2b656c48ab7fcd437789e1bbffcf7b1298372a77
Afolami Timothy Olawale <timmyafolami8469@gmail.com>
2024-04-12 18:28:21 +0100

    update-1

--- pages/Forecast_Analysis.py
+++ pages/Forecast_Analysis.py
@@ -11,9 +11,6 @@ from utils import create_dataset
 # loading in the data
 data = pd.read_csv('clean_data-2.csv')
 
-# setting the date column as integer
-data['year'] = data['year'].astype(int)
-
 # limiting the year column to 2020
 data_1 = data[data['year'] <= 2020]
 # another dataframe for the next 4 years
@@ -48,16 +45,16 @@ def filter_data(data_1, state, sector):
 
 # Filtered data based on user selection
 filtered_data = filter_data(data_1, state, sector)
-# This filtered data is for making foecasts
-filtered_data_2 = filter_data(data_2, state, sector)
+
 
 # Displaying the filtered data in a container
 with st.container():
     st.write(f"Data for {state} and {sector}")
     st.dataframe(filtered_data, use_container_width=True)
 
+st.subheader("Data Analysis")
 # A button to show some analysis 
-if st.button('Show Analysis'):
+if st.button('Show Analysis', key='analysis'):
     with st.container():
         st.write('Analysis of the data')
         fig, ax = plt.subplots(1, 2, figsize=(20, 6))
@@ -77,29 +74,35 @@ if st.button('Show Analysis'):
         st.write('Line Charts')
         st.line_chart(filtered_data[['price', 'revenue', 'sales', 'customers']])    
 
+st.subheader("Forecast Analysis")
 # A button to show the forecast
-if st.button('Show Forecast'):
+if st.button('Show Forecast', key='forecast'):
     with st.container():
         st.write('Forecast for the data till 2024')
         # Forecasting the price
-        X = filtered_data_2[['year', 'month', 'stateDescription', 'sectorName', 'customers', 'revenue', 'sales']]
+        X = data_2[['year', 'month', 'stateDescription', 'sectorName', 'customers', 'revenue', 'sales']]
         # encoding the categorical features [stateDescription, sectorName]
         cat_features = ['stateDescription', 'sectorName']
         X[cat_features] = cat_encoder.transform(X[cat_features])
         # scaling the features
-        X_scaled = feature_scaler.transform(X)
+        X_scaled = feature_scaler.transform(X.values)
         # the price column
-        y = filtered_data_2['price']
+        y = data_2['price']
         # reshaping the data for the model
-        X_scaled, y = create_dataset(X_scaled, y, look_back=3)
+        X_scaled, y = create_dataset(X_scaled, y)
         # making the forecast
         y_pred = model.predict(X_scaled)
+        # inverse scaling the forecasted price
         y_pred = price_scaler.inverse_transform(y_pred)
-        filtered_data['forecasted_price'] = y_pred
-        st.write(filtered_data[['year', 'month', 'price', 'forecasted_price']])
-
+        # reducing the dshape of filtered data to match the forecasted price
+        result_df = data_2.iloc[4:].reset_index(drop=True)
+        result_df['forecasted_price'] = y_pred
+        result_df['price'] = y
+        # filtering the data for the selected state and sector
+        result_df = result_df[(result_df['stateDescription'] == state) & (result_df['sectorName'] == sector)].reset_index(drop=True)
+        # plotting the actual and forecasted price
         fig, ax = plt.subplots(figsize=(15, 6))
-        sns.lineplot(x='year', y='price', data=filtered_data, ax=ax, label='Actual Price')
-        sns.lineplot(x='year', y='forecasted_price', data=filtered_data, ax=ax, label='Forecasted Price')
+        sns.lineplot(x='year', y='price', data=result_df, ax=ax, label='Actual Price')
+        sns.lineplot(x='year', y='forecasted_price', data=result_df, ax=ax, label='Forecasted Price')
         ax.set_title('Actual vs Forecasted Price')
         st.pyplot(fig)
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import numpy as np
-def create_dataset(X, y, look_back=1):
+def create_dataset(X, y, look_back=3):
     dataX, dataY = [], []
+    # making sure the data is in the right shape and it is a numpy array
+    X = np.array(X)
+    y = np.array(y)
     for i in range(len(X)-look_back-1):
         a = X[i:(i+look_back), :]
         dataX.append(a)
